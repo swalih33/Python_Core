e0140775ea00cc2456c072965f46c994970b2038
swalih33 <137264504+swalih33@users.noreply.github.com>
2023-07-05 22:42:15 +0530

    Add files via upload

---
+++ root/hello.py
@@ -0,0 +1,8 @@
+#defain the  function 
+def sum(a, b):
+    c = a + b
+    print("hello iam inside the root folder")
+    return c
+#calling the function
+# returned_value = sum(20, 20.5)
+# print("sum of a and b is: ", returned_value)
---
+++ sample.py
@@ -0,0 +1,7 @@
+
+from root.hello import sum
+
+total = sum(10, 20)
+print("total", total)
+print("hello")
+print("hai")
